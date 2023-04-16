e8bfeb137ae87587e6fc84d8fda7b70d47b805a6
Dayeon Yoon <yday1223@gmail.com>
2023-04-16 14:14:19 +0900

    dynamic route-pre-render HTML

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -7,3 +7,10 @@ type TParams = {
 export default function PantsPage({ params }: TParams) {
   return <h1>{params.slug} 제품 설명~</h1>;
 }
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
